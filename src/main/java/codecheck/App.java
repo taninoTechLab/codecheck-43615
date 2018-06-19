be942b793763fa63143b4838fc752f8998184e1e
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-19 17:59:06 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -8,10 +8,12 @@ public class App {
 			System.out.println(output);
 		}
 
-		if (args[0].equals("decode ")) {
+		if (args[0].equals("decode")) {
 			String base36 = returnNum(args[1]);
 			int dec = Integer.parseInt(base36, 9);//10進数に変換
 			System.out.println(String.valueOf(dec));
+		} else if (args[0].equals("encode")) {
+			System.out.println(returnAlphabet(args[1]));
 		}
 	}
 
@@ -37,6 +39,36 @@ public class App {
 			case 'H':
 				output.append('7');
 			case 'I':
+				output.append('8');
+			}
+		}
+		return output.toString();
+	}
+
+	static String returnAlphabet(String input) {
+		StringBuilder output = new StringBuilder();
+		int dec = Integer.parseInt(input);
+		String base9 = Integer.toString(dec, 9);
+		for (int i = 0, l = base9.length(); i < l; i++) {
+			switch (base9.charAt(i)) {
+
+			case '0':
+				output.append('0');
+			case '1':
+				output.append('1');
+			case '2':
+				output.append('2');
+			case '3':
+				output.append('3');
+			case '4':
+				output.append('4');
+			case '5':
+				output.append('5');
+			case '6':
+				output.append('6');
+			case '7':
+				output.append('7');
+			case '8':
 				output.append('8');
 			}
 		}
