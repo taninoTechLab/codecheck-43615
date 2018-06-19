d1541358f7d4b9ed9b3e7f1184dc3f6bd5cdd11c
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-19 17:49:16 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,44 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
+
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
 		}
+
+		if (args[0].equals("decode ")) {
+			String base36 = returnNum(args[1]);
+			int dec = Integer.parseInt(base36, 9);//9進数に変換
+			System.out.println(dec);
+		}
+	}
+
+	static String returnNum(String input) {
+		StringBuilder output = new StringBuilder();
+		for (int i = 0, l = input.length(); i < l; i++) {
+			switch (input.charAt(i)) {
+
+			case 'A':
+				output.append('0');
+			case 'B':
+				output.append('1');
+			case 'C':
+				output.append('2');
+			case 'D':
+				output.append('3');
+			case 'E':
+				output.append('4');
+			case 'F':
+				output.append('5');
+			case 'G':
+				output.append('6');
+			case 'H':
+				output.append('7');
+			case 'I':
+				output.append('8');
+			}
+		}
+		return output.toString();
 	}
 }
