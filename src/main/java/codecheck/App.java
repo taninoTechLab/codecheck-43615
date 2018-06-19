ec5a88b1e5a41e027a0f8e56104f7875e7cb77d1
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-19 18:11:17 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -27,22 +27,31 @@ public class App {
 			switch (input.charAt(i)) {
 			case 'A':
 				output.append('0');
+				break;
 			case 'B':
 				output.append('1');
+				break;
 			case 'C':
 				output.append('2');
+				break;
 			case 'D':
 				output.append('3');
+				break;
 			case 'E':
 				output.append('4');
+				break;
 			case 'F':
 				output.append('5');
+				break;
 			case 'G':
 				output.append('6');
+				break;
 			case 'H':
 				output.append('7');
+				break;
 			case 'I':
 				output.append('8');
+				break;
 			}
 		}
 		return output.toString();
@@ -57,22 +66,31 @@ public class App {
 
 			case '0':
 				output.append('A');
+				break;
 			case '1':
 				output.append('B');
+				break;
 			case '2':
 				output.append('C');
+				break;
 			case '3':
 				output.append('D');
+				break;
 			case '4':
 				output.append('E');
+				break;
 			case '5':
 				output.append('F');
+				break;
 			case '6':
 				output.append('G');
+				break;
 			case '7':
 				output.append('H');
+				break;
 			case '8':
 				output.append('I');
+				break;
 			}
 		}
 		return output.toString();
