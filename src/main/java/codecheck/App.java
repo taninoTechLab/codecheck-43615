6a5286a6c5918102d7c63a3e4d6e8331fd220bac
tie303053 <tie303053@S1711C12618-T1.tisnt.tis.co.jp>
2018-06-20 11:50:42 +0900

    test

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,21 +3,52 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 
-//		for (int i = 0, l = args.length; i < l; i++) {
-//			String output = String.format("argv[%s]: %s", i, args[i]);
-//			System.out.println(output);
-//		}
+		//		for (int i = 0, l = args.length; i < l; i++) {
+		//			String output = String.format("argv[%s]: %s", i, args[i]);
+		//			System.out.println(output);
+		//		}
 
 		if (args[0].equals("decode")) {
 			String base = returnNum(args[1]);
-			Long dec = Long.parseLong(base + "L", 9);//10進数に変換
+			Long dec = Long.parseLong(base, 9);//10進数に変換
 			System.out.println(String.valueOf(dec));
 
 		} else if (args[0].equals("encode")) {
 			System.out.println(returnAlphabet(args[1]));
 		} else if (args[0].equals("align")) {
-
+			System.out.println(returnAlphabet(args[1]));
 		}
+	}
+
+	static String calculateH(String input) {
+		StringBuilder output = new StringBuilder();
+		output.append(input).append(" + "); // 結果式の準備
+
+		String addStr = ""; //加算する数値の文字列
+		long add = 0; // 加算する数値
+		long result = 0; // 加算結果
+		String resultStr = ""; // 加算結果の文字列
+
+		String base = returnNum(input);
+		Long dec = Long.parseLong(base, 9);//10進数に変換
+
+		// 繰り返し加算してHになる数字を導く
+		boolean flag = false;
+		while (flag) {
+			result = dec + add;
+			resultStr = returnAlphabet(String.valueOf(result));
+			if (resultStr.matches("^[H]+$")) { // すべての文字がHに一致すれば終わり
+				addStr = returnAlphabet(String.valueOf(add));
+				flag = true;
+			}
+			add++;
+		}
+
+		return output
+				.append(addStr)
+				.append(" = ")
+				.append(returnAlphabet(resultStr))
+				.toString();
 	}
 
 	static String returnNum(String input) {
